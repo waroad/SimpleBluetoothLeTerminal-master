b276f5ebc70979bb121db7e608361ea074b63cd0
Doha Nam <waroad@knu.ac.kr>
2023-08-24 11:42:42 +0900

    Update blue_alimi.ino
    주석 추가 및 클린 코딩

--- blue_alimi/blue_alimi.ino
+++ blue_alimi/blue_alimi.ino
@@ -1,6 +1,11 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(4, 5);  // 소프트웨어 시리얼 (TX,RX)
+#define BLEHM10_PIN_TXD	10
+#define BLEHM10_PIN_RXD	11
+#define PUSHBUTTON_start	2
+#define PUSHBUTTON_stop	3
+#define PUSHBUTTON_reset	4
+SoftwareSerial BTSerial(BLEHM10_PIN_TXD, BLEHM10_PIN_RXD);  // 소프트웨어 시리얼 (TX,RX)
 
 int tt=0;   
 int dis_send_once=1;
@@ -8,37 +13,35 @@ unsigned long previousMillis = 0;  // Holds the last time the value was increase
 const unsigned long interval = 1000;
 void setup() {
   Serial.begin(9600);        // 통신속도 9600으로 설정
-  Serial.println("Hello!");  // 시리얼모니터에 Hello 출력
   BTSerial.begin(9600);
-  // BTSerial.write("AT+DISC?");
-  // BTSerial.write("AT+NAME?");
-  pinMode(7, INPUT);  
-  pinMode(8, INPUT);  
-  pinMode(2, INPUT);  
+  Serial.println("Hello!");  // 시리얼모니터에 Hello 출력
+  pinMode(PUSHBUTTON_start, INPUT);  
+  pinMode(PUSHBUTTON_stop, INPUT);  
+  pinMode(PUSHBUTTON_reset, INPUT);  
   previousMillis = millis();
 }
 
 void loop() {
-  bool start1 = digitalRead(7);
-  bool stop1 = digitalRead(8);
-  bool disconnect1 = digitalRead(2);
+  bool start1 = digitalRead(PUSHBUTTON_start);
+  bool stop1 = digitalRead(PUSHBUTTON_stop);
+  bool disconnect1 = digitalRead(PUSHBUTTON_reset);
   unsigned long currentMillis = millis(); 
   if (currentMillis - previousMillis >= interval) {
     dis_send_once=1;
     previousMillis = currentMillis;
   }
-  if (disconnect1 == HIGH && dis_send_once==1){
+  // dis_send_once를 이용해 disconnect가 최대 1초에 1번만 전송되도록
+  if (disconnect1 == HIGH && dis_send_once==1){ 
     BTSerial.write("disconnect");
     dis_send_once=0;
   }
+  // 똑같이 tt value를 이용해, start가 1번만 전송되고, stop이 눌려야 다시 전송 가능
   if (start1 == HIGH && tt==0) {
-    // Button was pressed
     Serial.print("hey\n");
     BTSerial.write("start");
     tt=1;
   }
   if (stop1 == HIGH && tt==1) {
-    // Button was not pressed
     Serial.print("stop\n");
     BTSerial.write("stop");
     tt=0;
@@ -47,7 +50,6 @@ void loop() {
     byte data = BTSerial.read();
     Serial.write(data);
   }
-
   while (Serial.available()) {
     byte data = Serial.read();
     BTSerial.write(data);
